c60446e4dadb09abafd75a996a6c9cf22fa40e1a
Matthew Judge <matthew.judge@bbc.co.uk>
2020-03-11 10:51:33 +0000

    Updates

---
+++ precache-manifest.58c8d63e46c33d43804bc76a91e499fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9cbf2d52936667db6ff03a1645a31f",
+    "url": "/covid-19-chart/index.html"
+  },
+  {
+    "revision": "3a225391653108d16d95",
+    "url": "/covid-19-chart/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "39548131c9929385b2b4",
+    "url": "/covid-19-chart/static/js/2.f9cc6434.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/covid-19-chart/static/js/2.f9cc6434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a225391653108d16d95",
+    "url": "/covid-19-chart/static/js/main.d3bdb6b4.chunk.js"
+  },
+  {
+    "revision": "b9811e4ff692d965627a",
+    "url": "/covid-19-chart/static/js/runtime-main.723407fb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19-chart/static/media/logo.5d5d9eef.svg"
+  }
+]);
